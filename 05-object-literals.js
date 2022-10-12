be672af1811b41b22c7d3d0b4f9b0b36b494d362
MarioChico <mario_chico98@hotmail.com>
2022-10-12 11:28:31 -0500

    Manera tradicional de pasar el nombre del objeto con el contenido que viene del mismo nombre

---
+++ 05-object-literals.js
@@ -0,0 +1,10 @@
+// enhanced object literals
+
+//Esta es la forma que lo escribiamos antes de, si el mismo nombre que estamos pasado regresa dentro de este objeto
+function newUser(user, age, country){
+    return {
+        user : user,
+        age: age,
+        country: country
+    }
+}
