98cc1ca84f5614d51fe8ceafc55d3c4d2ce8c859
MarioChico <mario_chico98@hotmail.com>
2022-10-13 12:24:02 -0500

    Archivos relacionados a los modulos, en conjunto con el package.json

---
+++ 08-module.js
@@ -0,0 +1,3 @@
+import hello from "./module.js";
+hello()
+ // Aqui falla porque hay que definir el valor que esta trabajando con un modulo package.son o nombrar el archivo con .mjs 08-module.mjs. Es necesario agregar la extension del archivo
